361858bfe29800a086474e426391e0ab96bf307d
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-03-16 14:38:29 +0400

    logic done

--- main.rb
+++ main.rb
@@ -1,15 +1,28 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
-require 'pry'
+require 'colorize'
 
+LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
 board = (1..9).to_a
-player_1 = []
-player_2 = []
-player_x = []
-player_y = []
+player1 = []
+player2 = []
 
 def cls
   Gem.win_platform? ? (system 'cls') : (system 'clear')
+end
+
+def check_winner(player_moves, symbol)
+  number = player_moves
+  new_lines = LINES.map do |line|
+    line.map! { |num| num == number ? symbol : num }
+  end
+
+  new_lines.each do |line|
+    if line.all? { |value| value == symbol }
+      puts "OK! It is WIN for #{symbol}"
+      abort
+    end
+  end
 end
 
 def display(board)
@@ -23,12 +36,16 @@ def display(board)
 end
 
 def display_x(board, player)
-  board[player - 1] = 'x'
+  cls
+  check_winner(player, 'x')
+  board[player - 1] = 'x'.colorize(:red)
   display(board)
 end
 
 def display_o(board, player)
-  board[player - 1] = 'O'
+  cls
+  check_winner(player, 'O')
+  board[player - 1] = 'O'.colorize(:yellow)
   display(board)
 end
 
@@ -38,38 +55,28 @@ p board
 
 until board.none? { |element| element.is_a?(Integer) }
   puts 'Player 1 = X Choose 1 to 9'
-  player_1 = gets.chomp.to_i
-  until board.include?(player_1)
-    puts "#{player_1} not availble"
-    player_1 = gets.chomp.to_i
+  player1 = gets.chomp.to_i
+  until board.include?(player1)
+    puts "#{player1} not availble"
+    player1 = gets.chomp.to_i
   end
 
-  if board.include?(player_1)
-    player_x << player_1
-    board[player_1 - 1] = player_1
-    cls
-    display_x(board, player_1)
-    p board
-    p player_x
+  if board.include?(player1)
+    board[player1 - 1] = player1
+    display_x(board, player1)
   end
 
-  if board.none? { |element| element.is_a?(Integer) }
-    abort "Draw"
-  end
+  abort 'No one won! Friendship !!! <3 <3 <3' if board.none? { |element| element.is_a?(Integer) }
 
   puts 'Player 2 = O Choose 1 to 9'
-  player_2 = gets.chomp.to_i
-  until board.include?(player_2)
-    puts "#{player_2} not availble"
-    player_2 = gets.chomp.to_i
+  player2 = gets.chomp.to_i
+  until board.include?(player2)
+    puts "#{player2} not availble"
+    player2 = gets.chomp.to_i
   end
 
-  if board.include?(player_2)
-    player_y << player_2
-    board[player_2 - 1] = player_2
-    cls
-    display_o(board, player_2)
-    p board
-    p player_y
-  end
+  next unless board.include?(player2)
+
+  board[player2 - 1] = player2
+  display_o(board, player2)
 end
